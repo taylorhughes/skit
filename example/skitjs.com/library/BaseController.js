6a3e4e8e60c3087847c09b94ab4104747bdb262b
Taylor Hughes <taylor@taylor-hughes.com>
2015-04-07 19:15:41 -0700

    Add GA tracking.

--- example/skitjs.com/library/BaseController.js
+++ example/skitjs.com/library/BaseController.js
@@ -9,5 +9,26 @@ var util = skit.platform.util;
 return Controller.create({
   __title__: function(childTitle) {
     return childTitle ? 'skit: ' + childTitle : 'skit';
+  },
+
+  __ready__: function() {
+    setTimeout(function() {
+      var parsed = urls.parse(window.location.href);
+      if (parsed.port && parsed.port != 80 && parsed.port != 443) {
+        return;
+      }
+
+      var GA_TRACKING_ID = 'UA-61684202-1';
+
+      var _gaq = window._gaq = window._gaq || [];
+      _gaq.push(['_setAccount', GA_TRACKING_ID]);
+      _gaq.push(['_trackPageview']);
+
+      var ga = document.createElement('script');
+      ga.type = 'text/javascript';
+      ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      document.getElementsByTagName('head')[0].appendChild(ga);
+    }, 100);
   }
 });
